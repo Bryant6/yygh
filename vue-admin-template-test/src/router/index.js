0834dce89b37f9a16379a86cb2ce8c8f972a7137
Bryant6 <1963899588@qq.com>
2021-11-18 15:05:38 +0800

    完成医院排班管理

--- vue-admin-template-test/src/router/index.js
+++ vue-admin-template-test/src/router/index.js
@@ -69,7 +69,21 @@ export const constantRouterMap = [
         component: () => import('@/views/hosp/show'),
         meta: { title: '查看', noCache: true },
         hidden: true
-      },      
+      },
+      {
+        path: 'hospital/schedule/:hoscode',
+        name: '排班',
+        component: () => import('@/views/hosp/schedule'),
+        meta: { title: '排班', noCache: true },
+        hidden: true
+      },
+      {
+        path: 'hospital/schedule/:hoscode',
+        name: '排班',
+        component: () => import('@/views/hosp/schedule'),
+        meta: { title: '排班', noCache: true },
+        hidden: true
+      },                  
       {
         path: 'hosp/list',
         name: '医院列表',
